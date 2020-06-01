cecd9ec9ce2e63252c097154eef3f950f6c941ff
bansalHritik <hb.rit345@gmail.com>
2020-06-01 19:22:48 +0530

    Drawer Navigator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
+import Home from "./HomeComponent";
 import DishDetail from "./DishDetailComponent";
 import { View, Platform } from "react-native";
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 // a stack naviagtor with some initial configs
+// a navigator means from this activity where we can go
 const MenuNavigator = createStackNavigator(
   {
+    // here order matters
     Menu: Menu, // can be written as below
-    DishDetail: { screen: DishDetail }, // may be something like routes 
+    DishDetail: { screen: DishDetail }, // may be something like routes
   },
   {
     initailRouteName: "Menu", // Default Screen
@@ -23,6 +25,44 @@ const MenuNavigator = createStackNavigator(
     },
   }
 );
+// we have used this to get the top title bar
+const HomeNavigator = createStackNavigator(
+  {
+    Home: Home, // can be written as below
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512D11", // color of header
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C$E(",
+  }
+);
 class Main extends Component {
   render() {
     return (
@@ -33,7 +73,7 @@ class Main extends Component {
             Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
         }}
       >
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
